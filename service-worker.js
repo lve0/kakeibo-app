de92285baa28e223c0928f81e0a76a4f4ec27e00
lve0 <fanshanneo@gmail.com>
2026-05-08 16:30:03 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-self.addEventListener('install', ()=>{});
+const CACHE_NAME='kakeibo-final-real-v3-20260508';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))})
